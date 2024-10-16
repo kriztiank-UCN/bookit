98f2281ada9308884fd8f03364062a1202ae7eb8
kk <kk@softnotik.dk>
2024-10-16 14:38:56 +0200

    Footer Component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
-import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,10 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header/>
+        <Header />
         <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
