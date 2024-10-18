573a7dbde7ef5a00df31451cbc220f302f11394d
kk <kk@softnotik.dk>
2024-10-18 09:25:38 +0200

    Get All Rooms

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,11 @@
-import RoomCard from "@/components/RoomCard";
 import Heading from "@/components/Heading";
-import rooms from "@/data/rooms";
+import RoomCard from "@/components/RoomCard";
+// import rooms from "@/data/rooms";
+import getAllRooms from "./actions/getAllRooms";
 
-export default function Home() {
+export default async function Home() {
+  const rooms = await getAllRooms();
+
   return (
     <>
       <Heading title="Available Rooms" />
