a802d82b47bdf00fe4bd40b4de82dff467395e2e
Kristian <10221425@ucn.dk>
2024-10-23 11:00:07 +0200

    Book Room Action

--- app/rooms/[id]/page.jsx
+++ app/rooms/[id]/page.jsx
@@ -69,8 +69,7 @@ const RoomPage = async ({ params }) => {
             </ul>
           </div>
         </div>
-
-        <BookingForm />
+        <BookingForm room={room} />
       </div>
     </>
   );
